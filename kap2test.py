7cc90cf0af489225b43d802cd292b63d9126728f
Jens Andreasson <jens.andreasson@ga.ntig.se>
2020-08-31 14:42:13 +0200

    Create kap2test.py

---
+++ kap2test.py
@@ -0,0 +1,15 @@
+v = 5
+print(v * v)
+
+name = input ('Ditt förnamn: ')
+surname = input ('Ditt efternamn: ')
+
+print('Hej ', name, ' ', surname)
+
+# skriv ett program som räknar * / + -s
+
+# print (23523.23 + 232 * 12 / 23423)
+
+# tal1 = input('Skriv ett tal? ')
+
+# print(tal1 + 2389572389)
